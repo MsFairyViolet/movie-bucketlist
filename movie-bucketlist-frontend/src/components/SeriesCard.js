834954f1561a76c42b8409524680699e7a4ebe19
Marieke <mariekewpas@gmail.com>
2025-03-03 11:45:07 +0100

    Unwatched Series functionality: if 1 movie is unwatched, the whole series is unwatched

--- movie-bucketlist-frontend/src/components/SeriesCard.js
+++ movie-bucketlist-frontend/src/components/SeriesCard.js
@@ -1,15 +1,15 @@
 import { WatchedSeriesGraphic } from "./WatchedSeriesGraphic"
-import { UnwatchedSeriesGraphic } from "./UnwatchedGraphic"
+import { UnwatchedSeriesGraphic } from "./UnwatchedSeriesGraphic"
 
 export default function SeriesCard({ series, showMovieInfo }) {
+
+    const allWatched = series.movies.every(movie => movie.watched)
+
     return (
         <div>
             <div className="series-card">
                 {/* Logic to check if all are watched */}
-
-                {/* {series.watched ? <WatchedSeriesGraphic series={series} /> : <UnwatchedSeriesGraphic series={series} />} */}
-
-                <WatchedSeriesGraphic series={series} />
+                {allWatched ? (<WatchedSeriesGraphic series={series} />) : (<UnwatchedSeriesGraphic series={series} />)}
             </div>
             {/* Logic to fix this button, so it is on every card and displays the correct information */}
             <button className="info-btn" onClick={() => showMovieInfo(series)}>ⓘ</button>
--- movie-bucketlist-frontend/src/components/UnwatchedSeriesGraphic.js
+++ movie-bucketlist-frontend/src/components/UnwatchedSeriesGraphic.js
@@ -1,12 +1,14 @@
+import SubMovieCard from "./SubMovieCard";
+
 export function UnwatchedSeriesGraphic({ series }) {
     return <div className="series-card unwatched-style">
         <div>
             {/* Logic to decide which title to display */}
-            <h4>{series[0].title}</h4>
-            <p>{series[0].year}</p>
+            <h4>{series.movies[0]?.title}</h4>
+            <p>{series.movies[0]?.year}</p>
         </div>
         <div className="sub-movies">
-            {series.map((movie) => {
+            {series.movies.map((movie) => {
                 return <SubMovieCard key={movie.id} movie={movie} />
             }
             )}
